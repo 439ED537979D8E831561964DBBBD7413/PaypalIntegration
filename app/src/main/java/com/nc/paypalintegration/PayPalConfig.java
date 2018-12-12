1579d0d5b961033b00d94a43e90f4ef09a58d8bf
neerajchouhan787 <neerajchouhan787@gmail.com>
2018-12-12 14:50:43 +0530

    Initial commit

---
+++ app/src/main/java/com/nc/paypalintegration/PayPalConfig.java
@@ -0,0 +1,5 @@
+package com.nc.paypalintegration;
+
+public class PayPalConfig {
+    public static final String PAYPAL_CLIENT_ID = "AcT3AHyW9dyez8kPN-QnBtuwWNQx1kZIzvHl3f6jcN6yIiZre8hQ21EOkgj3hicyZEp1N9LTNX_o2_K1";
+}
